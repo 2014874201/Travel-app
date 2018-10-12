a3a9b3619ad5f2ae4328e696684ee29f7a524f1b
2014874201 <1152579039@qq.com>
2018-10-12 20:11:32 +0800

    项目初始化

--- src/main.js
+++ src/main.js
@@ -3,9 +3,17 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import './assets/styles/border.css'
+
+// 1像素边框的解决方案
+import './assets/styles/reset.css'
+
+import fastClick from 'fastclick'
 
 Vue.config.productionTip = false
 
+// 绑定到body上，解决移动端300毫秒延迟（这是为了检查用户是否在做双击）
+fastClick.attach(document.body)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
